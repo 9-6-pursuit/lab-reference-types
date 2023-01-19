3bcfc6970a128d39cf36fcdc56b39b493d306f90
antunishdPursuit <dennysantunish@pursuit.org>
2023-01-18 23:02:54 -0500

    Answers

--- index.js
+++ index.js
@@ -4,7 +4,10 @@
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function addNewStore(stores, store) {}
+function addNewStore(stores, store) {
+  stores.push(store)
+  return stores
+}
 
 /**
  * Removes a store object at the given position.
@@ -12,14 +15,34 @@ function addNewStore(stores, store) {}
  * @param {number} index - A number representing the index of the store to be removed from the array.
  * @returns {Object[]} The same `stores` array that was inputted.
  */
-function removeStoreAtPosition(stores, index) {}
+function removeStoreAtPosition(stores, index) {
+  switch (index){
+    case 0:
+      stores.shift()
+    break;
+    default:
+      stores.splice(index, 1)
+  }
+  
+  return stores
+}
 
 /**
  * Creates a duplicate of the `store` object. No references should be shared between the inputted `store` and the result.
  * @param {Object} store - An object representing a single store. See the instructions for details on its shape.
  * @returns {Object} The duplicated store object. This should not be the same as the store that was inputted.
  */
-function duplicateStore(store) {}
+function duplicateStore(store) {
+  let clone = {}; // the new empty object
+
+  // let's copy all user properties into it
+  for (let key in store) {
+    clone.name = store.name;
+    clone.boardGames = [...store.boardGames];
+    clone.address = {...store.address};
+  }
+  return clone
+}
 
 module.exports = {
   addNewStore,
